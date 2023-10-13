cf1c69af96026af4f5b5d8f68bc88c291ccd322a
numonovnodirbek <nodirbeknumonov2007@gmail.com>
2023-10-13 11:31:04 +0500

    finished

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFoundP from "./pages/NotFoundP";
+import LayoutAdmin from "./components/layout";
+import StudentsP from "./pages/StudentsP";
+import TeachersP from "./pages/TeachersP";
+import LoginPage from "./pages/LoginP";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<LoginPage />} />
+        <Route path="/" element={<LayoutAdmin />}>
+          <Route path="students" element={<StudentsP />} />
+          <Route path="teachers" element={<TeachersP />} />
+        </Route>
+        <Route path="*" element={<NotFoundP />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/layout/index.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import {
+  GroupOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, Button, theme } from "antd";
+import { Link, Outlet } from "react-router-dom";
+const { Header, Sider, Content } = Layout;
+
+const LayoutAdmin = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [key, setKey] = useState(location.pathname);
+  useEffect(() => {
+    setKey(location.pathname);
+  }, []);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          selectedKeys={[key]}
+          onClick={({ key }) => {
+            setKey(key);
+          }}
+          items={[
+            {
+              key: "/teachers",
+              icon: <UserOutlined />,
+              label: <Link to="/teachers">Teachers</Link>,
+            },
+            {
+              key: "/students",
+              icon: <GroupOutlined />,
+              label: <Link to="/students">Students</Link>,
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default LayoutAdmin;
